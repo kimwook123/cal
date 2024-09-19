89c8c5d182ea6b2f0a4340cb75754d9ac8d2ffd6
kimwook123 <rladnr1467@naver.com>
2024-09-19 15:39:52 +0900

    bb

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,7 @@
 # tests/test_calculator.py
 
 import pytest
-from ..src.calculator import add, subtract, multiply, divide
+from src.calculator import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
